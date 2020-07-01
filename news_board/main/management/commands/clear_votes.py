e504555cc7ab3a5afda27672a35155fb76a43da1
oldgreek84 <oldgreek84@gmail.com>
2020-07-01 19:30:07 +0300

    add command to clear upvotes

---
+++ news_board/main/management/commands/clear_votes.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from main.models import Post
+
+
+class Command(BaseCommand):
+    help = '''' docstring '''
+
+    def handle(self, *args, **options):
+        posts = Post.objects.all()
+        for post in posts:
+            post.amount_of_upvotes = 0
+            post.save()
