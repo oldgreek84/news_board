6ed2c5b5baf9bc7d834bcd6d29667beb19d77c71
oldgreek84 <oldgreek84@gmail.com>
2020-07-01 23:29:23 +0300

    some

--- news_board/api/views.py
+++ news_board/api/views.py
@@ -12,27 +12,37 @@ from .serializers import CommentSerializer, PostSerializer
 
 
 class PostsView(ListCreateAPIView):
+    '''
+    class realise get, post methods
+    '''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def perform_create(self,  serializer):
+    def perform_create(self, serializer):
+        ''' method add author name to model'''
         author_name = self.request.data.get('author_name')
         return serializer.save(author_name=author_name)
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    ''' class realise put, patch, delete methods'''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class CommentsView(ListCreateAPIView):
+    ''' class realise get, post method  to models'''
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 class CommentDetail(RetrieveUpdateDestroyAPIView):
+    '''class realise put, patch, delete methods'''
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 class VotesUpdate(APIView):
+    ''' class realise change count of  upvotes in model'''
+
     def patch(self, request, pk):
+        ''' method find model by pk, and change model upvotes'''
         model = Post.objects.get(pk=pk)
         data = {'amount_of_upvotes': model.amount_of_upvotes + 1}
         serializer = PostSerializer(model, data=data, partial=True)
